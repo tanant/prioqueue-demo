7996bd560aed6b584dabb68c0995601e873ddc2f
Anthony Tan <anthony.tan@greenworm.net>
2024-12-08 18:00:04 -0500

    Update docstrings and run black

--- prioqueue.py
+++ prioqueue.py
@@ -1,17 +1,4 @@
-"""prioqueue.py
-
-initial notes - class, works as the scheduling logic behind executor. N
-
-no exec capacity, pure queue with accept/n'accept stuff
-
-assumes need for datetime or some kind of internal timing chop
-
-probably wa
-
-[ ]: testing
-
-
-for the purposes of the module, default design 
+"""Demonstrator class to accept command/priority pairs and maintain them in order.
 """
 
 import datetime
@@ -21,43 +8,54 @@ import collections
 
 LOGGER = logging.getLogger(__name__)
 
-# TODO: pushback in case of an accdiental pop?
 
 class PrioQueue(object):
-    """A container of commands to execute and their priority from 0
-    to the defined maximum - note that 0 is considered LOW 
-
-    time is from datetime and is stored for forensics purposes
-    
-    NOTE: does not deal with execution
+    """
+    A container of commands to execute and their priority from 0
+    to the defined maximum. 0 is considered low-priority (last to execute).
     """
 
-    def __init__(self, max_size: int = None, highest_priority: int = 10):
-        """Raises an exception for sizes < 1
-        does NOT raise an exception for a highest priority that is 0 which is the degenerate case of a list (but will warn)
-        
+    def __init__(self, max_size=None, highest_priority=10):
+        """Initialise the PrioQueue with range limits.
+
+        Note a highest priority of 0 _can_ be provided so this effectively
+        becomes a FIFO queue...
+
+        Args:
+            max_size: the limit of tasks this should store before raising an error (default None)
+            highest_priority: the maximum integer priority class (default: 10)
+
+        Raises:
+            ValueError: size requested < 1
         """
         if max_size is not None:
-            self.max_size = math.floor(max_size) 
+            self.max_size = math.floor(max_size)
             if self.max_size < 1:
-                raise ValueError("Max PrioQueue size requested is < 1") # that's.. not a queue.
+                raise ValueError(
+                    "Max PrioQueue size requested is < 1"
+                )  # that's.. not a queue.
         else:
             self.max_size = None
-        
+
         self._highest_priority = self._conform_priority(highest_priority, on_init=True)
         if self._highest_priority == 0:
             LOGGER.warning("Highest priority allowed is 0, this is effectively a list?")
 
-        self._task_dict = {x:collections.deque() for x in range(0,self.max_prio+1)}
+        self._task_dict = {x: collections.deque() for x in range(0, self.max_prio + 1)}
         self._taskcount = 0
 
-    def _conform_priority(self, priority: int, on_init=False):
-        """Given a priority, return the correct integerized number
-        
-        Effectively, this is a truncate-to-zero function with an upper
-        range set at module init time.
+    def _conform_priority(self, priority, on_init=False):
+        """Given a priority, return the correct integerized number.
 
-        Raises TypeError if you supply a non-conformable priority (like 'NOW')
+        Effectively, this is a truncate-to-zero function with an upper
+        value from the value provided at module init time.
+
+        Args:
+            priority: value to conform
+            on_init: whether or not to range the value back to thhe internal maxium for the class
+
+        Raises:
+            TypeError: if priority is non-conformable (like 'NOW')
         """
 
         # always range out negatives. this step will also catch the issue of
@@ -65,78 +63,83 @@ class PrioQueue(object):
         try:
             if priority < 0:
                 return 0
-        except TypeError: # 
+        except TypeError:  #
             raise TypeError(f"Priority '{priority}' is not a valid priority.")
-        
+
         # if at initialisation, we have no state information so let it through
         # TODO: find a more elegant way to do this. Maybe classmethod with a param
         #       but that's kinda overcooking the design I swear.
         if on_init:
             return math.floor(priority)
-          
+
         if priority > self.max_prio:
             return self.max_prio
         else:
             return math.floor(priority)
 
-    
-    ### admin/info functions
     @property
     def count(self):
-        """how many commands are stored?"""
+        """How many commands are stored?"""
         return self._taskcount
 
     @property
     def max_prio(self):
-        """what is the maxium priority
-        Protected as we don't want people to mangle max prio once instantiated."""
+        """what is the maxium priority level?
+
+        Explicitly protected as the datastructure relies on this being set at init
+        time and we cannot change it."""
         return self._highest_priority
-    
-    def _build_command(self, command, priority):
-        """Build out our command dict, including conforming the priority"""
 
     def push(self, command, priority):
-        """put the command into storage into it's correct priority ordering
-        
-        Raises ValueError if the queue is full.
-        
-        Returns the command dict with the timestamp accepted, this allows checking of the conformed priority"""
-        command_dict = {"command": command,
-         "priority": self._conform_priority(priority),
-         "add_time": datetime.datetime.now()}
-        
+        """Store a command, adding it to the correct priority ordering.
+
+        Returns:
+            A dict with the command, conformed priority and the timestamp accepted
+
+        Args:
+            command: the object (text string?) describing the command
+            priority: a numerical value for the command's desired priority
+
+        Raises:
+            ValueError: if the queue is full.
+        """
+        command_dict = {
+            "command": command,
+            "priority": self._conform_priority(priority),
+            "add_time": datetime.datetime.now(),
+        }
+
         if self.max_size is not None and self.count == self.max_size:
             raise ValueError(f"PrioQueue at capacity {self.max_size}, cannot add")
-        
+
         self._task_dict[command_dict["priority"]].append(command_dict)
-        self._taskcount+=1
+        self._taskcount += 1
 
         return command_dict
 
     def pop(self):
-        """Attempt to get the next task that this queue thinks should be executed. 
-        can return a Nonevalue
+        """Return the next command to be processed.
 
-        will return a command
+        Returns:
+            A dict with the command, conformed priority and the timestamp the command was or None if there are no tasks.
         """
-        
+
         # early bail, since the other case we walk backwards through the dict
         if self.count < 1:
             return None
 
         # walk backwards
-        for priority in range (self.max_prio, -1, -1):  # this really still bugs me I need a -1
+        for priority in range(
+            self.max_prio, -1, -1
+        ):  # this really still bugs me I need a -1
             try:
                 next_task = self._task_dict[priority].popleft()
             except IndexError:
-                continue # nothing in this prio level
-            self._taskcount-=1
+                continue  # nothing in this prio level
+            self._taskcount -= 1
             return next_task
-        
+
         # if you have a working self.count trap, this clause
         # can't be hit
         # else:
         #     return None
-
-
-    
--- test_prioqueue.py
+++ test_prioqueue.py
@@ -1,37 +1,34 @@
 import unittest
 import prioqueue
 import datetime
-
-
 import logging
 
 
 class TestPrioqueue(unittest.TestCase):
 
-
-
     def test_basic(self):
-        """can you even instantiate?"""
-        
-        # turn off the info/debug logging
+        """Can you even instantiate?"""
+
+        # turn off the info/debug logging for presentation purposes
         prioqueue.LOGGER.setLevel(logging.ERROR)
 
         # basic sanity checks
         with self.assertRaises(ValueError):
             prioqueue.PrioQueue(max_size=-1, highest_priority=10)
-    
+
         # use more detailed test setup later for complex cases, these are simple ranging
         # only checks
-        for init_max_size, init_highest_prio in [ 
-                (100,10), 
-                (10,-2),
-                (None,-2),
-            ]:
-            pq = prioqueue.PrioQueue(max_size=init_max_size, highest_priority=init_highest_prio)
+        for init_max_size, init_highest_prio in [
+            (100, 10),
+            (10, -2),
+            (None, -2),
+        ]:
+            pq = prioqueue.PrioQueue(
+                max_size=init_max_size, highest_priority=init_highest_prio
+            )
             self.assertEqual(pq.count, 0, msg="initialsize")
             self.assertEqual(pq.max_size, init_max_size, msg="capacity")
             self.assertEqual(pq.max_prio, max(0, init_highest_prio), msg="priority")
-
 
     def test_accept_task(self):
         """Check that tasks are added with the correct conformed priority code"""
@@ -39,103 +36,104 @@ class TestPrioqueue(unittest.TestCase):
         highest_priority = 7
         pq = prioqueue.PrioQueue(highest_priority=highest_priority)
 
-        # assert you couldn't add 
+        # assert you couldn't add
         with self.assertRaises(TypeError, msg="text priority accepted"):
-            pq.push("dostuff", "max")           
+            pq.push("dostuff", "max")
         self.assertEqual(pq.count, 0)
 
         # add something that's higher, make sure it's added _but_
         # that the priority was max only
-        result = pq.push("overprio", highest_priority+1)
-        self.assertEqual(result['priority'], highest_priority)
-        self.assertEqual(pq.count, 1) 
-        
+        result = pq.push("overprio", highest_priority + 1)
+        self.assertEqual(result["priority"], highest_priority)
+        self.assertEqual(pq.count, 1)
+
         result = pq.push("underprio", -1)
-        self.assertEqual(result['priority'], 0)
+        self.assertEqual(result["priority"], 0)
         self.assertEqual(pq.count, 2)
 
         # Deal with fractionals
         result = pq.push("split high", 3.9)
-        self.assertEqual(result['priority'], 3)
+        self.assertEqual(result["priority"], 3)
         self.assertEqual(pq.count, 3)
 
         result = pq.push("split low", 3.1)
-        self.assertEqual(result['priority'], 3)
+        self.assertEqual(result["priority"], 3)
         self.assertEqual(pq.count, 4)
 
         # test the timestamping is present and Close Enough
         # TODO: widen delta range if the queue get performance issues
         now = datetime.datetime.now()
         result = pq.push("timestamp", 3.1)
-        self.assertAlmostEqual(result['add_time'], now)
-
+        self.assertAlmostEqual(result["add_time"], now)
 
     def test_overflow(self):
-        """ there's gotta be a limit to tasks """
+        """There's gotta be a limit to tasks"""
         # instantiate with limit and then add past that
         # overflow logically follows accept
 
         max_size = 10
         pq = prioqueue.PrioQueue(max_size=max_size)
-        
+
         # no exception should be raised here
-        for x in range(0, max_size-1):
+        for x in range(0, max_size - 1):
             pq.push("dostuff", 0)
             self.assertNotEqual(pq.max_size, pq.count)
 
-        # now we're full, but no overflow issue
+        # now we're full, but no overflow yet, we should be fiiiine
         pq.push("dostuff", 0)
         self.assertEqual(pq.max_size, pq.count)
-            
+
+        # and _now_ we should overflow
         with self.assertRaises(ValueError, msg="overflow not trapped"):
             pq.push("dostuff", 0)
-        
-        self.assertEqual(pq.max_size, pq.count)
 
-        # TODO : find a task to make sure we didn't corrupt.. this is harder.
-    
+        self.assertEqual(pq.max_size, pq.count)
+        # TODO : find a task to make sure we didn't corrupt..
+        #        this is harder to do and might require internal inspection and is
+        #        implicitly done in the complex test below
 
     def test_underflow(self):
+        """Check that we correctly handle underflow as per spec"""
         pq = prioqueue.PrioQueue()
         self.assertEqual(pq.pop(), None, msg="popping from empty did not return None")
 
         pq.push("dostuff", 0)
-        self.assertNotEqual(pq.pop(), None, msg="popping from empty did not return None")
+        self.assertNotEqual(pq.pop(), None)
         self.assertEqual(pq.pop(), None, msg="popping from empty did not return None")
 
     def test_ordering(self):
-        """"""
+        """Check that our ordering follows both priority and insertion order"""
         pq = prioqueue.PrioQueue()
 
-        tasks = [("a",1),
-                ("b",2),
-                ("c",3),
-                ("d",4),
-                ("e",5),
-                ("f",6),
-                ("aa",1),
-                ("bb",2),
-                ("cc",3),
-                ("dd",4),
-                ("aaa",1),]
-        expected_order = ['f', 'e', 'd', 'dd', 'c', 'cc', 'b', 'bb', 'a', 'aa', 'aaa']
-        
+        tasks = [
+            {"command": "a", "priority": 1},
+            {"command": "b", "priority": 2},
+            {"command": "c", "priority": 3},
+            {"command": "d", "priority": 4},
+            {"command": "e", "priority": 5},
+            {"command": "f", "priority": 6},
+            {"command": "aa", "priority": 1},
+            {"command": "bb", "priority": 2},
+            {"command": "cc", "priority": 3},
+            {"command": "dd", "priority": 4},
+            {"command": "aaa", "priority": 1},
+        ]
+        expected_order = ["f", "e", "d", "dd", "c", "cc", "b", "bb", "a", "aa", "aaa"]
+
         for task in tasks:
-            pq.push(*task)
+            pq.push(**task)
 
         # would love a dump all, but that'd be just for testing purposes
         # and don't want more code there..
-        output_order = [pq.pop()['command'] for x in range(0, len(tasks))]
-        self.assertEqual(output_order,expected_order)
-        
+        output_order = [pq.pop()["command"] for x in range(0, len(tasks))]
+        self.assertEqual(output_order, expected_order)
+
         perturbed_tasks = tasks[::-1]
         for task in perturbed_tasks:
-            pq.push(*task)        
-        output_order = [pq.pop()['command'] for x in range(0, len(tasks))]
-        self.assertNotEqual(output_order, expected_order, msg="Tasks with same prio inserted in reverse order but output forward order")
-
-
-
-
-
-    
+            pq.push(**task)
+        output_order = [pq.pop()["command"] for x in range(0, len(tasks))]
+        self.assertNotEqual(
+            output_order,
+            expected_order,
+            msg="Tasks with same prio inserted in reverse order but output forward order",
+        )
